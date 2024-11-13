cd739bce66d522995bbbc71e501b893c1484cfd4
Pedro Lima <pedromlima.pm@gmail.com>
2024-11-13 13:55:51 -0300

    Inicio Aula 52 For

---
+++ Aula 52 for classico estruturaRepeticao/index.js
@@ -0,0 +1,4 @@
+//criando um for que exibe varias linhas em seguida, 1,2,3,4,5
+for(let i = 0; i <= 5; i++){      //Para um for é necessário, criar um variavel de controle com valor inicial, criar condição e criar um incrementador
+console.log(i)
+}
